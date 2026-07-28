a1f0ed80b31f155a82e52f76177636857bb92584
cat-black-brau <bcancamaal@gmail.com>
2026-07-27 21:23:25 -0500

    se agreagron comentarios

--- pryecto_final.py
+++ pryecto_final.py
@@ -1,12 +1,19 @@
+# Lista principal donde se almacenan todas las citas
 citas= []
 from datetime import datetime
 
+
+# Guarda todas las citas registradas en el archivo citas.txt
+# para conservar la informacion aunque se cierre el programa
 def guardar_citas():
     with open("citas.txt", "w", encoding="utf-8") as archivo:
         for cita in citas:
             linea = f"{cita['id']}|{cita['nombre']}|{cita['telefono']}|{cita['fecha']}|{cita['hora']}|{cita['motivo']}|{cita['doctor']}|{cita['observaciones']}|{cita['estado']}\n"
             archivo.write(linea)
 
+
+# Carga las citas almacenadas en el archivo citas.txt
+# y las guarda nuevamente en la lista citas al iniciar el sistem
 def cargar_citas():
     global citas
     citas = []
@@ -29,12 +36,18 @@ def cargar_citas():
                 citas.append(cita)
     except FileNotFoundError:
         citas = []
-        
+
+
+# Permite registrar una nueva cita medica
+# Solicita los datos del paciente, valida la fecha y hora,
+# genera un ID automatico y guarda la informacion
 def agregarcita():
     print('Nombre del paciente')
     nombre_paciente = input()
     print('Telefono:')
     telefono = input()
+    # Verifica que la fecha y hora ingresadas no sean anteriores
+    # a la fecha y hora actual del sistema
     while True:
         print('Fecha de la cita')
         print('(dd/mm/aaaa)')
@@ -60,9 +73,10 @@ def agregarcita():
     print('Observaciones:')
     observaciones = input()
     if len(citas) == 0:
-        id_cita = 1
+        id_cita = 1   # Genera un ID automatico para identificar cada cita
     else:
         id_cita = citas[-1]['id'] + 1
+    # Agrega la cita a la lista principal
     cita = {
         'id': id_cita,
         'nombre': nombre_paciente,
@@ -73,12 +87,15 @@ def agregarcita():
         'doctor': doctor,
         'observaciones': observaciones,
         'estado': 'Pendiente'}
-
+    # Guarda los cambios en el archivo de texto
     citas.append(cita)
     guardar_citas()
     print('*'*30)
     print('Cita agregada correctamente')
 
+
+# Muestra todas las citas registradas junto con su informacion
+# completa en pantalla
 def ver_cita():
     if len(citas) == 0:
         print('NO hay citas registradas')
@@ -96,11 +113,14 @@ def ver_cita():
             print("*"*30)
     regresar_menu()
 
+
+# Permite modificar una cita existente usando su ID
+# Se puede cambiar la fecha, hora, doctor o estado
 def modificar_cita():
     print("Ingrese el ID de la cita: ")
-    id_bucar =int(input())
-    for cita in citas:
-        if cita['id'] == id_bucar:
+    id_bucar =int(input())        
+    for cita in citas:           
+        if cita['id'] == id_bucar:  # Busca la cita correspondiente al ID ingresado
             print('¿Que deseas modificar?')
             print('1.Fecha')
             print("2. Hora")
@@ -108,23 +128,23 @@ def modificar_cita():
             print("4. Estado")
             print('selecciona una opcion')
             opcion = int(input())
-            match opcion:
+            match opcion:      # Actualiza la informacion seleccionada por el usuario
                 case 1 :
                     print('Integrese la nueva fecha')
                     print('(dd/mm/aaaa)')
-                    nueva_fecha = input() 
+                    nueva_fecha = input()      # Guarda los cambios realizados
                     cita['fecha'] = nueva_fecha
-                    print('*'*30)
+                    print('*'*30)     
                 case 2: 
                     print ('Ingrese el( nuevo horario')
                     print('(hh/mm)')
-                    nueva_hora = input()
+                    nueva_hora = input()   # Guarda los cambios realizados
                     cita['hora']= nueva_hora
                     print('*'*30)
                 case 3:
                     print ('¿A qué médico desea reasignar el caso?') 
                     nuevo_doctor= input()
-                    cita['doctor'] = nuevo_doctor
+                    cita['doctor'] = nuevo_doctor # Guarda los cambios realizados
                     print('*'*30)
                 case 4:
                     print('¿Cual sera el nuevo esta de la cita?')
@@ -134,14 +154,14 @@ def modificar_cita():
                     print('3. cancelada')
 
                     print('selecciona una opcion')
-                    estado = int(input())
+                    estado = int(input())  # Guarda los cambios realizados
                     match estado:
-                        case 1:
-                            cita['estado'] = "Pendiente"
+                        case 1: 
+                            cita['estado'] = "Pendiente"  # Guarda los cambios realizados
                         case 2: 
-                            cita ['estado'] = "Realizado"
+                            cita ['estado'] = "Realizado" # Guarda los cambios realizados
                         case 3:
-                            cita['estado'] = "Cancelado"
+                            cita['estado'] = "Cancelado" # Guarda los cambios realizados
                         case _:
                             print('Opcion no valida')
                 case _:
@@ -154,6 +174,8 @@ def modificar_cita():
     print('No se encontro una cita con ese ID')
     regresar_menu()
 
+
+# Busca una cita especifica mediante su ID y muestra toda la informacion relacionada
 def buscar_cita():
     print('*'*30)
     print('Ingrese el ID de la cita')
@@ -176,11 +198,16 @@ def buscar_cita():
     print('*'*30)
     print('No se encontro una cita con ese ID')
 
+
+# Muestra la cantidad total de citas registradas en el sistema
 def total_citas():
     print('*'*30)
     print('TOTAL DE CITAS REGISTRADAS')
     print(len(citas))
 
+
+# Genera un reporte con la cantidad de citas
+# pendientes, realizadas y canceladas
 def reporte_estados():
     print('*'*30)
     pendientes = 0
@@ -204,6 +231,8 @@ def reporte_estados():
     print(f'Realizadas: {realizadas}')
     print(f'Canceladas: {canceladas}')
 
+
+# Muestra el menu de reportes disponibles para el usuario
 def reportes():
 
     print('1. Total de citas')
@@ -228,6 +257,10 @@ def reportes():
             print('Opcion no valida')
             print('*'*30)
 
+
+# Funcion recursiva que pregunta al usuario si desea
+# continuar utilizando el sistema
+# Si la respuesta es invalida vuelve a llamarse a si misma
 def regresar_menu ():
    print('*'*30)
    VERDADERO  = 'SI'
@@ -242,11 +275,17 @@ def regresar_menu ():
    elif respuesta== FALSO:
       print('Ten un exelente dia :D')
    else:
-      print('Ingresa una respuesta valida ')   
+      print('Ingresa una respuesta valida ')
+      # Llamada recursiva para solicitar nuevamente
+      # una respuesta valida   
       regresar_menu()    
 
 
+# Menu principal del sistema
+# Permite acceder a todas las funcionalidades
+# disponibles para la gestion de citas
 def menu_opciones():
+    cargar_citas()
     print('¿Que le gustaria realizar el dia de hoy? ')
     print('1. Agregar una cita')
     print('2. Modificar una cita')
@@ -287,26 +326,28 @@ def menu_opciones():
           print('*'*30)
           print('Gracias por usar el sistema')
 
-print('*'*30)
-print('Bienvedio')
-print('*'*30)
-print('ingrese el usuario')
-usuario = input()
-print('*'*30)
-print('Ingrese la contraseña')
-contrasenia = input()
- 
+# Credenciales de acceso al sistema
 USUARIO = "RECEPCION" 
 PASSWORD = "1234"
 
-#comprobación del usuario y contrasenia 
-if usuario == USUARIO and contrasenia == PASSWORD:
-    cargar_citas()
-    print(f'Bienvedio {usuario}')
-    print("*"*30)
-    menu_opciones()
-else: 
-    print('***ERROR***')
-    print('Intenta denuevo')
+print('*'*30)
+print('Bienvenido')
+print('*'*30)
 
-#comentario
+
+# Verifica que el usuario y contraseña sean correctos
+# antes de permitir el acceso al sistema
+while True:    
+    print("Ingrese el usuario")
+    usuario = input()
+
+    print("Ingrese la contraseña")
+    contrasenia = input()
+
+    if usuario == USUARIO and contrasenia == PASSWORD:
+        print(f"Bienvenido {usuario}")
+        menu_opciones()
+        break  # <- sale del ciclo
+    else:
+        print("*** ERROR ***")
+        print("Intenta de nuevo")
